596615ccb312d43d941f65f4b147a87971535a13
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-04 10:45:23 -0400

    add all routes to router.

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
+  this.route('sign-up');
+  this.route('sign-in');
+  this.route('change-password');
+  this.route('users');
 });
 
 export default Router;

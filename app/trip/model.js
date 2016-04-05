64d70a2bc82971646ea627b2fc5c953731107ec0
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 14:22:27 -0400

    trips page working at a basic level.

--- app/trip/model.js
+++ app/trip/model.js
@@ -1,5 +1,19 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr('string'),
+  location: DS.attr('string'),
+  arrival: DS.attr('date'),
+  departure: DS.attr('date'),
+  description: DS.attr('string'),
+
+  // users: DS.hasMany('user', { async: true })
 });
+
+// t.string :title, null: false
+// t.string :location, null: false
+// t.date :arrival
+// t.date :departure
+// t.text :description
+//
+// t.timestamps null: false

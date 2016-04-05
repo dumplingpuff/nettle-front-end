3564473ff1e4b3f031dfc2cab80331a2701c2a06
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 14:52:38 -0400

    successful load on trip page

--- app/router.js
+++ app/router.js
@@ -12,7 +12,7 @@ Router.map(function () {
   this.route('change-password');
   this.route('users');
   this.route('trips');
-  this.route('trip');
+  this.route('trip', {path: '/trips/:trip_id'});
 });
 
 export default Router;

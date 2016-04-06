40bd616977bfbae03683d222b7fe8d3b8ca4a85a
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 20:40:24 -0400

    Create destroy method for trips page

--- app/components/trips-snippet/component.js
+++ app/components/trips-snippet/component.js
@@ -2,4 +2,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    destroyTrip: function(){
+      console.log('Component Action : destroyTrip');
+      this.sendAction('routeDestroyTrip', this.get('trip'));
+    }
+  },
 });

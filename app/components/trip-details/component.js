136929b0b94edd9f2ff20d205841adcf9bc7ce92
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-06 13:53:00 -0400

    Able to edit trip on trip page

--- app/components/trip-details/component.js
+++ app/components/trip-details/component.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isEditable: false,
-  toggleEditable: function() {
+
+  actions: {
+    toggleEditable: function() {
+      this.toggleProperty('isEditable');
+    },
+    updateTrip: function(){
+      console.log('Component Action : updateTrip');
+      this.sendAction('routeUpdateTrip', this.get('trip'));
+      this.set('isEditable', false);
+    },
+  },
+  doubleClick: function(){
     this.toggleProperty('isEditable');
   },
 });
--- app/trip-create/route.js
+++ app/trip-create/route.js
@@ -4,11 +4,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-  createTrip: function(properties){
-    console.log('Route Action : createTrip');
-    this.store.createRecord('trip', properties)
-      .save().then(()=>console.log('record created'));
-    this.transitionTo('trips')
-  },
-}
+    createTrip: function(properties){
+      console.log('Route Action : createTrip');
+      this.store.createRecord('trip', properties)
+        .save().then(()=>console.log('record created'));
+      this.transitionTo('trips');
+    },
+  }
 });

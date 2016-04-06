71752db778d2d7b55dd6ee50e9ef822360bc3d66
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 22:40:30 -0400

    generate trip form component

---
+++ app/trip-create/route.js
@@ -0,0 +1,13 @@
+// create trip route
+
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+  createTrip: function(properties){
+    console.log('Route Action : createTrip');
+    this.store.createRecord('trip', properties)
+      .save().then(()=>console.log('record created'));
+  },
+}
+});

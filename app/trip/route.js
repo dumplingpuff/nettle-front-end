57672fa147684cc3f0ee35fd3d17f0ccb33b0f27
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-07 08:47:01 -0400

    Able to create item without association

--- app/trip/route.js
+++ app/trip/route.js
@@ -5,10 +5,19 @@ export default Ember.Route.extend({
   model (params) {
     return this.store.findRecord('trip', params.trip_id);
   },
+  item:{},
   actions: {
     updateTrip: function(trip) {
       console.log('Route Action: update Trip');
       trip.save();
+    },
+    createItem(item) {
+      // console.log(model);
+      // console.log(this.get('trip'));
+      console.log('Route Action: create item');
+      this.store.createRecord('item', item)
+        .save().then(()  => console.log('record created'));
+      // model.items.pushObject(newItem);
     }
   }
 });
@@ -19,3 +28,4 @@ export default Ember.Route.extend({
 //   items: this.store.findAll('item')
 // });
 // return this.store.findAll('item');
+// model.items.push

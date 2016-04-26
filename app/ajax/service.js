328429c03a654a9f076fd1c0831e71e4b8dd1412
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-25 22:55:33 -0400

    add host to ajax service and adapter

--- app/ajax/service.js
+++ app/ajax/service.js
@@ -3,6 +3,7 @@ import AjaxService from 'ember-ajax/services/ajax';
 
 export default AjaxService.extend({
   auth: Ember.inject.service(),
+  host: 'https://floating-temple-20684.herokuapp.com',
   headers: Ember.computed('auth.credentials.token', {
     get() {
       let headers = {};
--- app/application/adapter.js
+++ app/application/adapter.js
@@ -3,7 +3,7 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   auth: Ember.inject.service(),
-
+  host: 'https://floating-temple-20684.herokuapp.com',
   headers: Ember.computed('auth.credentials.token', {
     get() {
       let headers = {};

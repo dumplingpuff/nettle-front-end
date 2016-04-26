c937b2a1a25db4a51094ff88d6fb6013a4051b72
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-26 10:13:32 -0400

    departure and arrival dates create with plugin

--- app/components/trip-form/component.js
+++ app/components/trip-form/component.js
@@ -7,7 +7,8 @@ export default Ember.Component.extend({
       console.log('Component Action: createTrip');
       console.log('This is the form');
       // console.log(this.get('form'));
-      this.set('form.arrival', this.get('arrival'));
+      this.set('form.arrival', new Date(this.get('arrival')));
+      this.set('form.arrival', new Date(this.get('departure')));
       console.log(this.get('form'));
       this.sendAction('routeCreateTrip', this.get('form'));
       this.set('form', {});
--- dist/assets/nettle-front-end.js
+++ dist/assets/nettle-front-end.js
@@ -2073,7 +2073,8 @@ define('nettle-front-end/components/trip-form/component', ['exports', 'ember'], 
         console.log('Component Action: createTrip');
         console.log('This is the form');
         // console.log(this.get('form'));
-        this.set('form.arrival', this.get('arrival'));
+        this.set('form.arrival', new Date(this.get('arrival')));
+        this.set('form.arrival', new Date(this.get('departure')));
         console.log(this.get('form'));
         this.sendAction('routeCreateTrip', this.get('form'));
         this.set('form', {});

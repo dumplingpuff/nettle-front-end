8ee267dac5bec30117cca2e3b2da8b1f59352b5d
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-27 15:29:20 -0400

    fix update typo

--- app/item/model.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  content: DS.attr('string'),
-  trip: DS.belongsTo('trip', { async: true }),
-  user: DS.belongsTo('user', { async: true })
-});
